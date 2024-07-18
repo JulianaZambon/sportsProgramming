1373330020544b1c3734b6518eaed531f7bf1563
Juliana Zambon <64793722+JulianaZambon@users.noreply.github.com>
2024-07-17 23:15:10 -0300

    Update base.cpp

--- base.cpp
+++ base.cpp
@@ -1,9 +1,14 @@
 #include <bits/stdc++.h>
+#include <iostream>
 using namespace std;
 
-int main () {
+#define ll long long
+
+int main() {
     ios_base::sync_with_stdio(0);
     cin.tie(0);
+
+    ll n;
     /* code here */
 
     return 0;
