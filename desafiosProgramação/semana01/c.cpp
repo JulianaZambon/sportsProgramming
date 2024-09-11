370aaa5c485cac250ea76a81a9317938f993ed92
Juliana Zambon <64793722+JulianaZambon@users.noreply.github.com>
2024-09-10 22:27:37 -0300

    Update c.cpp

--- desafiosProgramação/semana01/c.cpp
+++ desafiosProgramação/semana01/c.cpp
@@ -7,5 +7,17 @@ int main()
     ios_base::sync_with_stdio(0);
     cin.tie(0);
 
+    int n, q;  /* tamanho do texto e qntd de consultas */
+    cin >> n >> q;
+    string s; /* texto s com n caracteres latinos */
+    cin >> s;
+
+    /* seguem q consultas cada uma em uma linha */
+    /* linha da consulta = t (tipo da consulta) e x */
+    /* se t == 1 o inteiro X identifica quantas vezes a operação deve ser repetida */
+    /* se t == 2 qual o caractere a ser impresso do texto */
+
+    /* sempre vai haver pelo menos uma consulta do tipo 2 */
+
     return 0;
 }
